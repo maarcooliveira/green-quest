138c38ce28e1e31d0f61b8e7356f3a36f0973a5f
Marco Oliveira <oliveira.marco.andre@gmail.com>
2015-10-09 00:53:43 +0000

    project base

---
+++ public/js/controllers.js
@@ -0,0 +1,13 @@
+var demoControllers = angular.module('demoControllers', []);
+
+demoControllers.controller('GameController', ['$scope', '$rootScope', '$window', function($scope, $rootScope, $window) {
+
+}]);
+
+demoControllers.controller('RankingController', ['$scope', '$rootScope', '$routeParams', '$window', function($scope, $rootScope, $routeParams, $window) {
+
+}]);
+
+demoControllers.controller('MainController', ['$scope', '$rootScope', '$routeParams', '$window', function($scope, $rootScope, $routeParams, $window) {
+
+}]);
---
+++ public/js/services.js
@@ -0,0 +1,15 @@
+//var baseUrl = "http://localhost:4000/api";
+var baseUrl = "";
+
+angular.module('demoServices', [])
+    .factory('Questions', function($http, $window){
+        return {
+
+        }
+    })
+    .factory('Ranking', function($http, $window){
+        return{
+
+        }
+    })
+    ;
