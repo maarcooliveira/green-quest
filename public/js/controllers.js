a0f2c468feb9f36f5b5b54a165d1888236761359
marco <oliveira.marco.andre@gmail.com>
2015-10-10 10:59:58 -0300

    pagina de perguntas definida; nova versao do materialize

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,13 +1,1 @@
-var demoControllers = angular.module('demoControllers', []);
-
-demoControllers.controller('GameController', ['$scope', '$rootScope', '$window', function($scope, $rootScope, $window) {
-
-}]);
-
-demoControllers.controller('RankingController', ['$scope', '$rootScope', '$routeParams', '$window', function($scope, $rootScope, $routeParams, $window) {
-
-}]);
-
-demoControllers.controller('MainController', ['$scope', '$rootScope', '$routeParams', '$window', function($scope, $rootScope, $routeParams, $window) {
-
-}]);
+var demoControllers=angular.module("demoControllers",[]);demoControllers.controller("GameController",["$scope","$rootScope","$window",function($scope,$rootScope,$window){}]),demoControllers.controller("RankingController",["$scope","$rootScope","$routeParams","$window",function($scope,$rootScope,$routeParams,$window){}]),demoControllers.controller("MainController",["$scope","$rootScope","$routeParams","$window",function($scope,$rootScope,$routeParams,$window){}]);
--- public/js/services.js
+++ public/js/services.js
@@ -1,15 +1,1 @@
-//var baseUrl = "http://localhost:4000/api";
-var baseUrl = "";
-
-angular.module('demoServices', [])
-    .factory('Questions', function($http, $window){
-        return {
-
-        }
-    })
-    .factory('Ranking', function($http, $window){
-        return{
-
-        }
-    })
-    ;
+var baseUrl="";angular.module("demoServices",[]).factory("Questions",function($http,$window){return{}}).factory("Ranking",function($http,$window){return{}});
